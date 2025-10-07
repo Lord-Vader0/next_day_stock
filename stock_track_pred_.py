24982a5f6c6436fd81f9dae3331791b04dc32cff
Vineeth <145290222+Lord-Vader0@users.noreply.github.com>
2025-10-07 14:45:19 -0400

    Add files via upload

--- stock_track_pred_.py
+++ stock_track_pred_.py
@@ -6,16 +6,18 @@ from sklearn.linear_model import LinearRegression as LinReg
 def main():
     while True:
 
-        stock_name = input("Enter stock tickers seperated by commas (no spaces)").upper()
+        stock_name = input("Enter stock tickers seperated by commas (no spaces): ").upper()
 
         tickers = []
         raw_list = stock_name.split(',')
         for stock in raw_list:
             tickers.append(stock)
         for ticker in tickers:
-            current, next_day_pred = pred_price(ticker)
-            print(f"The current price for {ticker} is ${current:.2f} and the estimated price tomorrow at closing is ${next_day_pred:.2f}")
-    
+            try:
+                current, next_day_pred = pred_price(ticker)
+                print(f"The current price for {ticker} is ${current:.2f} and the estimated price tomorrow at closing is ${next_day_pred:.2f}")
+            except Exception as e:
+                print(f"Error processing {ticker}: {e}")
 
 def pred_price(ticker):
     hist = yf.download(ticker, period = '120d')
@@ -28,8 +30,6 @@ def pred_price(ticker):
     current = float(hist["Close"].iloc[-1])
     next_day_pred = stock_model.predict([[current]]).item()
     return current, next_day_pred
-
-main()
 
 if __name__ == "__main__":
     main()
